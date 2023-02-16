3d9860ede9598a259dc32929a7af98eeb934bda8
krups88 <b88pl88@gmail.com>
2023-02-16 14:09:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>dsds</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
