c89b5654fb44aaeba08a1deb20efcad791663698
krups88 <b88pl88@gmail.com>
2023-02-21 20:16:42 +0100

    new projekt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { Header } from './Header';
-import { Technologies } from './Technologies';
+
 
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://www.planetware.com/wpimages/2019/11/canada-in-pictures-beautiful-places-to-photograph-morraine-lake.jpg'/>
+        </header>
+        <nav className='nav'>
+        <a>Profile</a>
+        <a>Messages</a>
+        <a>News</a>
+        <a>Music</a>
+        <a>Settings</a>
+        </nav>
+        <div className='content'>
+          <img src='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg'/>
+        </div>
     </div>
   );
 }
