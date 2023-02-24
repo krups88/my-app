3c45e22c788941e6e0c862fcb10efcfe3c3fa806
krups88 <b88pl88@gmail.com>
2023-02-24 17:32:26 +0100

    props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -12,11 +12,9 @@ const MyPosts = () => {
                 New post
             </div>
             <div className={s.posts}>
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                <Post message='Hi, how are you?' like='15'/>
+                <Post message="it's my first post" like='20'/>
+
             </div>
         </div>
     )
