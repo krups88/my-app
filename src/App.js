08518579a96a49c5e382c247a434ae90f8df42d7
krups88 <b88pl88@gmail.com>
2023-03-07 21:16:17 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 
 
-
-
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div className='app-wrapper'>
-        <Header />
-        <Navbar />
-        <div class='app-wrapper-content'>
-          <Routes>
-            <Route  path='/dialogs' element={<Dialogs />} />
-            <Route  path='/profile' element={<Profile />} />
-            <Route  path='/news' element={<News />} />
-            <Route  path='/music' element={<Music />} />
-            <Route  path='/settings' element={<Settings />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>);
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div class='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>);
 }
 
 export default App;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,25 +1,7 @@
 import React from "react";
 import s from './Dialogs.module.css';
-import {NavLink} from 'react-router-dom';
-
-
-const DialogItem = (props) => {
-
-    let path = '/dialogs/' + props.id;
-
-    return (
-        <div className={s.dialog + ' ' + s.active}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-
-const Message = (props) => {
-    return (
-        <div className={s.dialog}>{props.message}</div>
-    )
-}
+import {Message} from "./Message/Message";
+import {DialogItem} from "./DialogItem/Dialogitem";
 
 
 const Dialogs = (props) => {
